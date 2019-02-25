3bc943406ecbd3a7f8c7363520db3daba70ec9a7
daniel.carretero@gmail.com <daniel.carretero@gmail.com>
2019-02-25 15:58:43 +0100

    Unifying criteria of posting

--- Post-1-Data Manipulation Comparative SQL_R_Python/SQL/create_table_Airport_Frequencies.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE AIRPORTS_FREQUENCIES(
- id INT ,
- airport_ref INT NOT NULL,
- airport_ident VARCHAR (200) NOT NULL,
- type VARCHAR(200),
- description VARCHAR (2000),
- frequency_mhz NUMERIC
-);
--- Post-1-Data Manipulation Comparative SQL_R_Python/SQL/load_data_frequencies.sql
+++
@@ -1,2 +0,0 @@
-COPY RUNWAYS(id,airport_ref,airport_ident,length_ft,width_ft,surface,lighted,closed,le_ident,le_latitude_deg,le_longitude_deg,le_elevation_ft,le_heading_degT,le_displaced_threshold_ft,he_ident,he_latitude_deg,he_longitude_deg,he_elevation_ft,he_heading_degT,he_displaced_threshold_ft)
-FROM 'e:\Source Code\Pagina Personal\Post-1-Data Manipulation Comparative SQL_R_Python\Data\runways.csv' DELIMITER ',' CSV HEADER;
