3e5563c64dd74e4b839f5688a19231d54c562db9
daniel.carretero@gmail.com <daniel.carretero@gmail.com>
2019-03-11 16:05:33 +0100

    Ended R notebook

--- Post -Data Manipulation Comparative SQL, Python and R/SQL/12_Over_partition_by.sql
+++ Post -Data Manipulation Comparative SQL, Python and R/SQL/12_Over_partition_by.sql
@@ -1,4 +1,4 @@
---11.1) MAX LATITUDE VALUE BY TYPE OF AIRPORT
+--12.1) MAX LATITUDE VALUE BY TYPE OF AIRPORT
 SELECT distinct TYPE,max(latitude_deg) over (partition BY TYPE ) FROM airports
 ORDER BY type
 
